f74d179f0b5748ea6294440afb93d0c162ad1cc5
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-08-21 08:18:17 -0400

    Create Get-ADGroup_added_full.ps1

---
+++ Get-ADGroup_added_full.ps1
@@ -0,0 +1,41 @@
+# Step 1: Specify the AD Group
+$groupName = "Domain Admins"
+
+# Step 2: Query the group to get its members
+$membersList = Get-ADGroupMember -Identity $groupName | Select-Object -ExpandProperty SamAccountName
+#$membersList = @("test1", "Administrator") #SamAccountName
+
+# Output the list of members (for debugging purposes)
+#Write-Host "Members of ${groupName}:"
+#$membersList
+
+# Step 3: Use the list of users in your script
+$group = Get-ADGroup -Filter { Name -eq $groupName }
+
+if ($group -eq $null) {
+    Write-Host "Group ${groupName} not found."
+    exit
+}
+
+$members = Get-ADGroupMember -Identity $groupName
+
+# Create an array to hold the results
+$result = @()
+
+foreach ($member in $members) {
+    if ($member.objectClass -eq "user") {
+        if ($membersList -contains $member.SamAccountName) {
+            $user = Get-ADUser -Identity $member -Properties whenCreated
+            # Add user details to the result array
+            $result += [pscustomobject]@{
+                Name          = $user.Name
+                SamAccountName = $user.SamAccountName
+                WhenCreated   = $user.whenCreated
+            }
+        }
+    }
+}
+
+# Output the results in a formatted table
+$result | Format-Table -AutoSize
+#$result | Export-Csv -Path "C:\temp\GroupMembers.csv" -NoTypeInformation
