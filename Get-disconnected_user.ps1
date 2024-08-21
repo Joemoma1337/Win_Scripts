70a62addcd49d83e6aa36aacb32a5193d74696a0
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-08-21 08:37:04 -0400

    Update Get-disconnected_user.ps1

--- Get-disconnected_user.ps1
+++ Get-disconnected_user.ps1
@@ -1,6 +1,9 @@
-param (
-	[string[]]$Hostname
-)
+#for use syntax: .\Get-Disconnected.ps1 -Hostname <hostname>
+#param (
+#	[string[]]$Hostname
+#)
+
+$Hostname = @("hostname1") #comment out if using param
 foreach ($i in $Hostname) {
 	Write-Output $i
 	query user /server:$i | Select-String -Pattern "USERNAME|disc"
