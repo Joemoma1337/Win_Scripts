37defcd1ac975818758d8a78bb5ed523f0ebc166
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2026-01-13 13:17:21 -0500

    Update Enum-Active_Workstations.ps1

--- Enum-Active_Workstations.ps1
+++ Enum-Active_Workstations.ps1
@@ -1,3 +1,4 @@
+#query active workstations (non-server)
 Get-ADComputer -Filter {(OperatingSystem -Notlike "*windows*server*") -and (Enabled -eq "True")} -Properties OperatingSystem, OperatingSystemVersion | 
     Select-Object DNSHostName, OperatingSystem, OperatingSystemVersion | 
     Export-Csv -Path "C:\temp\all_non_server_endpoints.csv" -NoTypeInformation
